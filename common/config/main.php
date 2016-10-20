461a0db78bd6750470d28d52167c833c5747c74e
a7000q@gmail.com <a7000q@gmail.com>
2016-10-20 06:40:26 +0300

    auth

--- common/config/main.php
+++ common/config/main.php
@@ -6,7 +6,14 @@ return [
     'modules' => [
         'user' => [
             'class' => 'dektrium\user\Module',
-            'admins' => ['admin']
+            'admins' => ['admin'],
+            'mailer' => [
+                'sender'                => 'rail555@yandex.ru', // or ['no-reply@myhost.com' => 'Sender name']
+                'welcomeSubject'        => 'Welcome subject',
+                'confirmationSubject'   => 'Confirmation subject',
+                'reconfirmationSubject' => 'Email change subject',
+                'recoverySubject'       => 'Recovery subject',
+            ],
             // you will configure your module inside this file
             // or if need different configuration for frontend and backend you may
             // configure in needed configs
