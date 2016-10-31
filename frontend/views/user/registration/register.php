24a09d0f313aa2c412cedb288faec4b1cd79abf5
a7000q@gmail.com <a7000q@gmail.com>
2016-10-31 11:57:42 +0300

    register

--- frontend/views/user/registration/register.php
+++ frontend/views/user/registration/register.php
@@ -65,7 +65,7 @@ $this->params['breadcrumbs'][] = $this->title;
                                 <?= $form->field($model, 'password')->passwordInput() ?>
                             <?php endif ?>
 
-                            <?= \himiklab\yii2\recaptcha\ReCaptcha::widget(['name' => 'reCaptcha']) ?>
+                            <?= \himiklab\yii2\recaptcha\ReCaptcha::widget(['name' => 'reCaptcha', 'siteKey' => '6LfZswoUAAAAALpMaBml8eOrB5PgpcKdFR40G3kQ']) ?>
 
                             <?= Html::submitButton(Yii::t('user', 'Sign up'), ['class' => 'btn btn-success btn-block']) ?>
 
